d293af2fcf6e41bfb02d4e9ecb0d062c65e0a67c
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-10-24 17:56:01 -0300

    projeto criado

---
+++ src/main/java/br/com/fiap/trafego/TrafegoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.trafego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafegoApplication.class, args);
+	}
+
+}
