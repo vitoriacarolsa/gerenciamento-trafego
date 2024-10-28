7962ea67cba8940dde3e5d6fc842ce6bb23a6212
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-10-27 23:52:29 -0300

    fix : trafego

--- src/main/java/br/com/fiap/trafego/controllers/TrafegoController.java
+++ src/main/java/br/com/fiap/trafego/controllers/TrafegoController.java
@@ -1,13 +1,15 @@
 package br.com.fiap.trafego.controllers;
 
+import br.com.fiap.trafego.dto.SemaforoDTO;
 import br.com.fiap.trafego.dto.TrafegoDTO;
 import br.com.fiap.trafego.services.TrafegoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +23,20 @@ public class TrafegoController {
     public ResponseEntity<List<TrafegoDTO>> findAll(){
         List<TrafegoDTO>list= trafegoService.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping (value= "/{id}")
+    public ResponseEntity<TrafegoDTO> findById(@PathVariable long id){
+        TrafegoDTO dto= trafegoService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity <TrafegoDTO> insert(@Valid @RequestBody TrafegoDTO dto){
+        dto= trafegoService.insert(dto);
+        URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.id()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
 
--- src/main/java/br/com/fiap/trafego/services/TrafegoService.java
+++ src/main/java/br/com/fiap/trafego/services/TrafegoService.java
@@ -1,10 +1,9 @@
 package br.com.fiap.trafego.services;
 
-import br.com.fiap.trafego.dto.SemaforoDTO;
 import br.com.fiap.trafego.dto.TrafegoDTO;
-import br.com.fiap.trafego.entities.Semaforo;
 import br.com.fiap.trafego.entities.Trafego;
 import br.com.fiap.trafego.repositories.TrafegoRepository;
+import br.com.fiap.trafego.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +20,30 @@ public class TrafegoService {
     public List<TrafegoDTO> findAll(){
         List<Trafego> result = trafegoRepository.findAll();
         return result.stream().map(x->new TrafegoDTO(x)).toList();
+    }
+
+    @Transactional
+    public TrafegoDTO insert (TrafegoDTO dto){
+        Trafego entity= new Trafego();
+        copyDtoToEntity (dto, entity);
+        entity= trafegoRepository.save(entity);
+        return new TrafegoDTO(entity);
+    }
+
+
+    @Transactional(readOnly = true)
+    public TrafegoDTO findById(Long id){
+        Trafego trafego = trafegoRepository.findById(id).orElseThrow
+                (() -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new TrafegoDTO(trafego);
+    }
+
+    private void copyDtoToEntity(TrafegoDTO dto, Trafego entity){
+       entity.setCongestionamento(dto.congestionamento());
+       entity.setDataHora(dto.dataHora());
+       entity.setLocalizacao(dto.localizacao());
+       entity.setVelocidadeVeiculo(dto.velocidadeVeiculo());
+       entity.setQuantidadeVeiculo(dto.quantidadeVeiculo());
+
     }
 }
